ab269a3e0ddd07a815ac4b33ad2c6a4899801dd6
jane <jane@teamturing.com>
2022-02-08 00:11:38 +0900

    기하학&이분탐색

--- A와B2_12919.py
+++ A와B2_12919.py
@@ -1,9 +1,24 @@
-S = input()
-T = input()
+S = list(input())
+T = list(input())
 
 
-def AB(str1, str2):
-    if str1 == str2:
-        return 1
+def AB(S, T):
+    str1 = T
+    str2 = T
+    if len(S) == len(T):
+        if str1 == S or str2 == S:
+            print(1)
+            return
     else:
-        if list(str2).pop()
+        str1.pop()
+        str2.reverse()
+        str2.pop()
+        AB(S, str1)
+        AB(S, str2)
+
+
+AB(S, T)
+print(0)
+
+
+
--- 사다리_2022.py
+++ 사다리_2022.py
@@ -1,8 +1,26 @@
 import sys
-from sympy import Symbol, solve
+
 case = sys.stdin.readline().strip().split()
-x, y, c = int(case[0]), int(case[1]), int(case[2])
-k = (x**2 - y**2) / c**2
-a = Symbol('a')
-equation = k * a ** 2 * (1 - a) ** 2 - 2 * a + 1
-solve(equation)
+x, y, c = float(case[0]), float(case[1]), float(case[2])
+
+
+def 교점높이(x, y, k):
+    y_1 = (y**2 - k**2)**0.5
+    x_1 = (x**2 - k**2)**0.5
+    h = x_1 * y_1 / (x_1 + y_1)
+    return h
+
+l = 0
+r = min(x, y)
+while True:
+    mid = (l + r) / 2
+    if abs(교점높이(x, y, mid) - c) < 10**(-3):
+        print(mid)
+        break
+
+    else:
+        if 교점높이(x, y, mid) > c + 10**(-3):
+            l = mid
+        elif 교점높이(x, y, mid) < c - 10**(-3):
+            r = mid
+
