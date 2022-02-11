2d39b51b3788e2bb6994710f54a0f84d9fd620c0
jane <jane@teamturing.com>
2022-02-12 01:45:48 +0900

    기하학 & 브루트포스

--- 고층건물_1027.py
+++ 고층건물_1027.py
@@ -1,33 +1,58 @@
 import sys
+
 N = int(input())
 case = sys.stdin.readline().strip().split()
 A = [0 for _ in range(N)]
 for i in range(N):
     A[i] = int(case[i])
-H = [[0 for _ in range(N)] for _ in range(N)]
-for i in range(N):
-    for j in range(N):
+H = [[0 for _ in range(N+1)] for _ in range(N+1)]
+for i in range(1, N+1):
+    for j in range(1, N+1):
         if i == j:
             H[i][j] = 0
         else:
-            H[i][j] = (A[i] - A[j]) / (i - j)
-my_list = [0 for _ in range(N)]
-for i in range(N):
-    Sum1 = 0
-    Sum2 = 0
-    for j in range(i):
-        DP = [1 for _ in range(N)]
-        for k in range(i):
-            for l in range(k):
-                if H[i][k] < H[i][l]:
-                    DP[k] = max(DP[k], DP[l] + 1)
-        Sum1 = max(DP)
-    for j in range(i, N):
-        DP = [1 for _ in range(N)]
-        for k in range(i, N):
-            for l in range(i, k):
-                if H[i][k] > H[i][l]:
-                    DP[k] = max(DP[k], DP[l] + 1)
-        Sum2 = max(DP)
-    my_list[i] = Sum1 + Sum2
+            H[i][j] = (A[i-1] - A[j-1]) / (i - j)
+my_list = [1 for _ in range(N+1)]
+for i in range(1, N + 1):
+    if N == 1:
+        print(0)
+        exit(0)
+    Sum1 = 1
+    Sum2 = 1
+    if i == 1:
+        Max = H[i][i + 1]
+        for j in range(i+1, N+1):
+            if H[i][j] > Max:
+                my_list[i] += 1
+                Max = H[i][j]
+    elif i == N:
+        Min = H[i][i - 1]
+        for j in range(i - 1, -1, -1):
+            if H[i][j] < Min:
+                my_list[i] += 1
+                Min = H[i][j]
+            if j == 1:
+                if H[i][j] < Min:
+                    my_list[i] += 1
+                break
+
+    else:
+        Min = H[i][i - 1]
+        for j in range(i-1, -1, -1):
+            if H[i][j] < Min:
+                Sum1 += 1
+                Min = H[i][j]
+            if j == 1:
+                if H[i][j] < Min:
+                    Sum1 += 1
+                break
+
+        Max = H[i][i + 1]
+        for j in range(i+1, N+1):
+            if H[i][j] > Max:
+                Sum2 += 1
+                Max = H[i][j]
+        my_list[i] = Sum1 + Sum2
+
 print(max(my_list))
+
