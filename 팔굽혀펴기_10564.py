04ddf7f8841ec04249a827581fbd32e426a184c5
jane <jane@teamturing.com>
2022-02-17 20:28:57 +0900

    DP

--- 팔굽혀펴기_10564.py
+++ 팔굽혀펴기_10564.py
@@ -6,17 +6,16 @@ for i in range(T):
     score = []
     for j in range(M):
         score.append(int(case[j]))
-    DP = [[0 for _ in range(501)] for _ in range(N + 1)]
+    DP = [[0 for _ in range(502)] for _ in range(5001)]
     for j in score:
         DP[j][j] = j
-    for l in range(N + 1):
-        for j in range(l + 1):
-            if j <= 500:
-                for k in score:
-                    if j - k >= 0 and DP[l - j][j - k] != 0:
-                        DP[l][j] = max(DP[l][j], DP[l - j][j - k] + k)
-                    else:
-                        continue
+    for l in range(1, N + 1):
+        for j in range(min(500, l)):
+            for k in score:
+                if j - k >= 0 and DP[l - j][j - k] != 0:
+                    DP[l][j] = max(DP[l][j], DP[l - j][j - k] + k)
+                else:
+                    continue
     result = max(DP[N])
     if result == 0:
         print(-1)
