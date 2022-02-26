b7c3bce515ec8c335170c91cb0d29e518a03abdd
jane <jane@teamturing.com>
2022-02-26 22:06:59 +0900

    자료구조

---
+++ 절댓값힙_11286.py
@@ -0,0 +1,14 @@
+import heapq
+import sys
+heap = []
+N = int(sys.stdin.readline().strip())
+for i in range(N):
+    x = int(sys.stdin.readline().strip())
+    if x == 0:
+        if heap:
+            result = heapq.heappop(heap)[1]
+            print(result)
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, (abs(x), x))
