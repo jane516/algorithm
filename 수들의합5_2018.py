8d985fe5347feb3ad1a54b51eb6f5b786c1495b9
jane <jane@teamturing.com>
2022-03-08 20:59:33 +0900

    수학 & 두포인터

--- 수들의합5_2018.py
+++ 수들의합5_2018.py
@@ -11,4 +11,9 @@ for i in range(N // 2):
         elif sum_list[j] == sum_list[i] + N:
             count += 1
 count += sum_list.count(N)
-print(count + 1)
+if N == 1:
+    print(1)
+elif N == 2:
+    print(1)
+else:
+    print(count + 1)
