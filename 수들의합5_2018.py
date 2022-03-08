aebe4f132f523bb17b62b39da0569dc6a379ff49
jane <jane@teamturing.com>
2022-03-08 20:57:36 +0900

    수학 & 두포인터

--- 수들의합5_2018.py
+++ 수들의합5_2018.py
@@ -1,18 +1,14 @@
 import sys
 N = int(sys.stdin.readline().strip())
-num_list = [i for i in range(1, N + 1)]
-sum_list = [0 for _ in range(N)]
-sum_list[0] = 1
+sum_list = [i * (i + 1) // 2 for i in range(1, N // 2 + 2)]
+
 count = 0
 
-for i in range(1, N):
-    sum_list[i] += num_list[i] + sum_list[i - 1]
-
-for i in range(N - 1):
-    for j in range(i + 1, N):
+for i in range(N // 2):
+    for j in range(i, N // 2 + 1):
         if sum_list[j] > sum_list[i] + N:
             break
         elif sum_list[j] == sum_list[i] + N:
             count += 1
 count += sum_list.count(N)
-print(count)
+print(count + 1)
