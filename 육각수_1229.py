1ca1d6434c3b2b1d9fd2adad556913ff716c3faf
jane <jane@teamturing.com>
2022-03-14 01:57:30 +0900

    ì수학 & DP

--- 육각수_1229.py
+++ 육각수_1229.py
@@ -1,13 +1,16 @@
 import sys
 N = int(sys.stdin.readline().strip())
 육각수 = [2 * i**2 - i for i in range(1, 709)]
-count = 0
-while N > 0:
-    for i in range(len(육각수)):
-        if 육각수[i] > N:
-            N -= 육각수[i-1]
-            print(육각수[i-1])
-            count += 1
+
+dp = [6 for _ in range(N + 1)]
+dp[0] = 0
+
+
+for i in range(708):
+    for j in range(육각수[i], N + 1):
+        if 육각수[i] <= j:
+            dp[j] = min(dp[j], dp[j - 육각수[i]] + 1)
+        else:
             break
-print(count)
-print(육각수)
+
+print(dp[N])
