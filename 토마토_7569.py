d205ad88c041ee21a690ccae962794d1274fedaf
jane <jane@teamturing.com>
2022-03-17 20:01:56 +0900

    그래프이론 & BFS

--- 토마토_7569.py
+++ 토마토_7569.py
@@ -3,42 +3,36 @@ from collections import deque
 M, N, H = map(int, input().split())
 queue = deque([])
 result = 0
-tomato = [[0 for _ in range(M)] for _ in range(N * H)]
-for i in range(N * H):
-    case = sys.stdin.readline().strip().split()
-    for j in range(M):
-        tomato[i][j] = int(case[j])
-dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
-for i in range(N * H):
-    for j in range(M):
-        if tomato[i][j] == 1:
-            queue.append([i, j])
+tomato = [[[0 for _ in range(M)] for _ in range(N)] for _ in range(H)]
+for i in range(H):
+    for j in range(N):
+        case = sys.stdin.readline().strip().split()
+        for k in range(M):
+            tomato[i][j][k] = int(case[k])
+dx, dy, dz = [-1, 1, 0, 0, 0, 0], [0, 0, -1, 1, 0, 0], [0, 0, 0, 0, -1, 1]
+for i in range(H):
+    for j in range(N):
+        for k in range(M):
+            if tomato[i][j][k] == 1:
+                queue.append([i, j, k])
 
 
 def bfs():
     while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx, ny = dx[i] + x, dy[i] + y
-            for j in range(H):
-                if j * N <= nx < (j + 1) * N and 0 <= ny < M and tomato[nx][ny] == 0:
-                    tomato[nx][ny] = tomato[x][y] + 1
-                    queue.append([nx, ny])
-                if (j + 1) * N <= x + N < (j + 2) * N and x + N < N * H:
-                    if tomato[x + N][y] == 0:
-                        tomato[x + N][y] = tomato[x][y] + 1
-                        queue.append([x + N, y])
-                if (j - 1) * N <= x - N < j * N and x - N >= 0:
-                    if tomato[x - N][y] == 0:
-                        tomato[x - N][y] = tomato[x][y] + 1
-                        queue.append([x - N, y])
+        x, y, z = queue.popleft()
+        for i in range(6):
+            nx, ny, nz = dx[i] + x, dy[i] + y, dz[i] + z
+            if 0 <= nx < H and 0 <= ny < N and 0 <= nz < M and tomato[nx][ny][nz] == 0:
+                tomato[nx][ny][nz] = tomato[x][y][z] + 1
+                queue.append([nx, ny, nz])
 
 
 bfs()
-for i in range(N * H):
-    for j in range(M):
-        if tomato[i][j] == 0:
-            print(-1)
-            exit(0)
-        result = max(result, tomato[i][j])
+for i in range(H):
+    for j in range(N):
+        for k in range(M):
+            if tomato[i][j][k] == 0:
+                print(-1)
+                exit(0)
+            result = max(result, tomato[i][j][k])
 print(result-1)
