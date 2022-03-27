bf55f442e4c55c0407e0e58b75936397ba701a9b
jane <jane@teamturing.com>
2022-03-27 20:00:07 +0900

    비트마스킹

--- 단어암기_18119.py
+++ 단어암기_18119.py
@@ -2,22 +2,25 @@ import sys
 case = sys.stdin.readline().strip().split()
 N, M = int(case[0]), int(case[1])
 my_list = []
-check_list = []
 for i in range(N):
     word = sys.stdin.readline().strip()
-    my_list.append([word, 0])
+    bitmask = 0
+    for c in word:
+        idx = ord(c) - ord('a')
+        bitmask |= (1 << idx)
+    my_list.append(bitmask)
+check = 2**26 - 1
 for i in range(M):
-    count = 0
     case2 = sys.stdin.readline().strip().split()
-    if case2[0] == '1':
-        for j in range(N):
-            if case2[1] in my_list[j][0]:
-                my_list[j][1] += 1
+    o, x = case2[0], case2[1]
+    idx = ord(x) - ord('a')
+    if o == '1':
+        check &= ~(1 << idx)
     else:
-        for j in range(N):
-            if case2[1] in my_list[j][0]:
-                my_list[j][1] -= 1
-    for j in range(N):
-        if my_list[j][1] == 0:
+        check |= (1 << idx)
+    count = 0
+    for j in my_list:
+        if check & j == j:
             count += 1
     print(count)
+
