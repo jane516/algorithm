6ced9df5dfca478f553a73d4a5772ecd3adddd57
jane <jane@teamturing.com>
2022-03-30 00:02:41 +0900

    DP

---
+++ 동물원_1309.py
@@ -0,0 +1,8 @@
+import sys
+N = int(sys.stdin.readline().strip())
+dp = [0 for _ in range(N + 1)]
+dp[0] = 1
+dp[1] = 3
+for i in range(2, N + 1):
+    dp[i] = (2 * dp[i-1] + dp[i-2]) % 9901
+print(dp[N])
