0462a68284f6d7dd1f15f7adc1847aed40c55de3
jane <jane@teamturing.com>
2022-04-01 16:24:53 +0900

    구현 & 문자열

--- 추월_2002.py
+++ 추월_2002.py
@@ -1,10 +1,14 @@
 N = int(input())
-in_car = []
-out_car = []
-else_car = []
+in_car = {}
+visited = [0 for _ in range(N)]
+count = 0
 for i in range(N):
-    in_car.append(input())
-for j in range(N):
+    in_car[input()] = i
+for i in range(N):
     X = input()
-    if X == in_car[j]:
-        out_car.append(X)
+    visited[in_car[X]] = 1
+    if in_car[X] > i:
+        count += 1
+    elif in_car[X] <= i and sum(visited[:in_car[X] + 1]) != in_car[X] + 1:
+        count += 1
+print(count)
