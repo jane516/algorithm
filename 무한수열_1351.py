62c878c9eb6384dd3896eec16b9ec72893991478
jane <jane@teamturing.com>
2022-04-04 23:00:34 +0900

    수학

--- 무한수열_1351.py
+++ 무한수열_1351.py
@@ -1,17 +1,15 @@
 import sys
-case = sys.stdin.read().strip().split()
+case = sys.stdin.readline().strip().split()
 N, P, Q = int(case[0]), int(case[1]), int(case[2])
-dp = [0 for _ in range(10**5)]
-dp[0] = 1
-dp[1] = 2
-for i in range(1000):
-    n, m = i, i
-    while True:
-        if n % P < P:
-            break
-        n //= P
-    while True:
-        if m % Q < Q:
-            break
-        m //= Q
-    dp[i] = dp[n]
+dic = {0: 1}
+
+
+def A(n):
+    if n in dic:
+        return dic[n]
+    else:
+        dic[n] = A(n//P) + A(n//Q)
+        return dic[n]
+
+
+print(A(N))
