bc5536f4ec4edd4a2964a36f4751bb4d52625e9d
jane <jane@teamturing.com>
2022-04-20 09:56:57 +0900

    DP

---
+++ 1,2,3더하기4_15989.py
@@ -0,0 +1,10 @@
+T = int(input())
+dp = [0 for _ in range(10001)]
+dp[1] = 1
+dp[2] = 2
+dp[3] = 3
+for i in range(4, 10001):
+    dp[i] = dp[i-3] + i // 2 + 1
+for i in range(T):
+    n = int(input())
+    print(dp[n])
