e488f49415989c59a39108997114c4a3669520f5
jane <jane@teamturing.com>
2022-05-01 16:28:30 +0900

    DP & BFS

--- 1로만들기_12852.py
+++ 1로만들기_12852.py
@@ -1,12 +1,20 @@
+from collections import deque
 N = int(input())
-dp = [0] * 1000001
-dp[1] = 0
-A = []
-for i in range(2, N + 1):
-    dp[i] = dp[i-1] + 1
-    A.append(N-i)
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i//2] + 1)
-    if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i//3] + 1)
-print(dp[N])
+if N == 1:
+    print(0)
+    print(1)
+else:
+    dp = [0] * (N + 1)
+    queue = deque()
+    queue.append((1, [1]))
+    while queue:
+        X, Y = queue.popleft()
+        dx = [X + 1, 2 * X, 3 * X]
+        for nx in dx:
+            if nx <= N and dp[nx] == 0:
+                if nx == N:
+                    print(dp[X] + 1)
+                    result = Y + [nx]
+                    print(*result[::-1])
+                queue.append((nx, Y + [nx]))
+                dp[nx] = dp[X] + 1
