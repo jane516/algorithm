91f3755a28fe7bb85dd68857e152bfa5faf654f4
jane <jane@teamturing.com>
2022-05-13 20:35:23 +0900

    tree

--- 전화번호목록_5052.py
+++ 전화번호목록_5052.py
@@ -1,15 +1,16 @@
+import sys
 t = int(input())
 for i in range(t):
-    n = int(input())
+    n = int(sys.stdin.readline().strip())
     phone_list = []
     for j in range(n):
         number = input()
-        phone_list.append((len(number), number))
+        phone_list.append(number)
     phone_list.sort()
     check = 0
     for j in range(n-1):
-        for k in range(j+1, n):
-            if phone_list[j][1] == phone_list[k][1][:len(phone_list[j][1])]:
+        if len(phone_list[j]) < len(phone_list[j+1]):
+            if phone_list[j+1][:len(phone_list[j])] == phone_list[j][:]:
                 check = 1
                 break
     if check == 0:
