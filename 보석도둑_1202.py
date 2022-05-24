a82a0d31df894013108e56104cafd45309fc1d55
jane <jane@teamturing.com>
2022-05-24 20:57:16 +0900

    우선순위큐

--- 보석도둑_1202.py
+++ 보석도둑_1202.py
@@ -1,14 +1,20 @@
 import sys
+import heapq
 N, K = map(int, input().split())
-VM_list = [[0 for _ in range(2)] for _ in range(N)]
-C_list = [[0 for _ in range(2)] for _ in range(K)]
-for i in range(N):
-    case = sys.stdin.readline().strip().split()
-    VM_list = int(case[1]), int(case[0])
-for i in range(K):
-    C_list[i][0] = int(sys.stdin.readline().strip())
-
-VM_list.sort()
+보석 = []
+for _ in range(N):
+    heapq.heappush(보석, list(map(int, sys.stdin.readline().strip().split())))
+C_list = []
+for _ in range(K):
+    C_list.append(int(sys.stdin.readline().strip()))
+C_list.sort()
 result = 0
-for i in range(N):
-    V, M = VM_list.pop()
+V_list = []
+for i in C_list:
+    while 보석 and i >= 보석[0][0]:
+        heapq.heappush(V_list, -heapq.heappop(보석)[1])
+    if V_list:
+        result -= heapq.heappop(V_list)
+    elif not 보석:
+        break
+print(result)
