3154f8c8cdcefedb382e85d3855deb32c4ee87d7
jane <jane@teamturing.com>
2022-06-01 20:57:12 +0900

    ã수학

--- 점분리_3878.py
+++ 점분리_3878.py
@@ -6,12 +6,14 @@ for _ in range(T):
     for i in range(n):
         x, y = map(int, sys.stdin.readline().strip().split())
         bxy_list[i][0], bxy_list[i][1] = x, y
+    bxy_list.sort(key=lambda x: (x[1], x[0]))
+    a, b = bxy_list[0][0], bxy_list[0][1]
     wxy_list = [[0, 0, 1] for _ in range(m)]
     for i in range(m):
         x, y = map(int, sys.stdin.readline().strip().split())
         wxy_list[i][0], wxy_list[i][1] = x, y
-    xy_list = bxy_list + wxy_list
-    x, y = 0, 0
+    wxy_list.sort(key=lambda x: (x[1], x[0]))
+    c, d = wxy_list[0][0], wxy_list[0][1]
 
 
     def cross_product(x1, y1, x2, y2, x3, y3):
@@ -51,7 +53,7 @@ for _ in range(T):
                 j += 1
         arr += list1[i:]
         arr += list2[j:]
-        return
+        return arr
 
 
     check = 0
@@ -59,7 +61,6 @@ for _ in range(T):
     if xy_list[0][2] == 0:
         for j in range(n):
             if xy_list[j][2] == 1:
-                print('NO')
                 check = 1
                 break
         if check == 0:
@@ -67,8 +68,20 @@ for _ in range(T):
     else:
         for j in range(m):
             if xy_list[j][2] == 0:
-                print('NO')
                 check = 1
                 break
         if check == 0:
             print('YES')
+    if check == 1:
+        bxy_list.sort()
+        wxy_list.sort()
+        if bxy_list[-1][0] <= wxy_list[0][0] or wxy_list[-1][0] <= bxy_list[0][0]:
+            print('YES')
+        else:
+            bxy_list.sort(key=lambda x: (x[1], x[0]))
+            wxy_list.sort(key=lambda x: (x[1], x[0]))
+            if bxy_list[-1][1] <= wxy_list[0][1] or wxy_list[-1][1] <= bxy_list[0][1]:
+                print('YES')
+            else:
+                print('NO')
+
