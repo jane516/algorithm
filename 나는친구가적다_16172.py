09a3c3d47abb9e5e7c269d1eb1144d5d087f5871
jane516 <rsvp0516@naver.com>
2022-09-13 20:32:07 +0900

    문자열

--- 나는친구가적다_16172.py
+++ 나는친구가적다_16172.py
@@ -1,9 +1,11 @@
-import re
 import sys
 S = sys.stdin.readline().strip()
 K = sys.stdin.readline().strip()
-S = re.sub(r'[0-9]', '', S)
-if K in S:
+result = ''
+for s in S:
+    if s.isalpha():
+        result += s
+if K in result:
     print(1)
 else:
     print(0)
