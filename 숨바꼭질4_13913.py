28d08c26d9e026c87a52f3b19b0a52345ca9b4f6
jane516 <rsvp0516@naver.com>
2022-12-28 18:49:01 +0900

    BFS

--- 숨바꼭질4_13913.py
+++ 숨바꼭질4_13913.py
@@ -1,19 +1,28 @@
+import sys
 from collections import deque
 
-N, K = map(int, input().split())
-visited = [0 for _ in range(200000)]
-
-queue = deque([N])
+N, K = map(int, sys.stdin.readline().strip().split())
+v_s = [-1 for _ in range(100001)]
+v_s[N] = 0
+queue = deque([(N, 0)])
 
 while queue:
-    X = queue.popleft()
+    X, t = queue.popleft()
+
     if X == K:
+        print(t)
+        arr = [X]
+        while X != N:
+            X = v_s[X]
+            arr.append(X)
+        print(*list(reversed(arr)))
         break
+    dx = [X - 1, X + 1, 2 * X]
 
-    이동 = [X - 1, X + 1, 2 * X]
-    for i in 이동:
-        if 0 <= i < 200000 and not visited[i]:
-            visited[i] = visited[X] + 1
-            queue.append(i)
-
-print(visited[K])
+    for i in dx:
+        if not 0 <= i <= 100000:
+            continue
+        if v_s[i] != -1:
+            continue
+        v_s[i] = X
+        queue.append((i, t + 1))
