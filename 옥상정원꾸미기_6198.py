7b7f5e461b6daba4da97f8d42418c32fe77afb24
jane <jane@teamturing.com>
2023-02-05 14:14:18 +0900

    stack

--- 옥상정원꾸미기_6198.py
+++ 옥상정원꾸미기_6198.py
@@ -1,19 +1,21 @@
 import sys
 
 N = int(sys.stdin.readline().strip())
-A_list = [0 for _ in range(N)]
+buildings = [0 for _ in range(N)]
 
 for i in range(N):
-    A_list[i] = int(sys.stdin.readline().strip())
-stack = []
-result = 0
+    buildings[i] = int(sys.stdin.readline().strip())
 
-for i in range(N):
-    while stack and A_list[stack[-1]] < A_list[N - 1 - i]:
-        result += 1
-        stack.pop()
-    stack.append(N - 1 - i)
 
-if len(A_list) > 1 and A_list[0] > A_list[1]:
-    result += 1
-print(result)
+def bench_marking(N, buildings):
+    result = [0] * N
+    stack = []
+    for i in range(N):
+        while stack and buildings[stack[-1]] <= buildings[i]:
+            stack.pop()
+        result[i] = len(stack)
+        stack.append(i)
+    return sum(result)
+
+
+print(bench_marking(N, buildings))
