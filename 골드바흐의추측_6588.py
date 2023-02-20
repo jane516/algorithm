dd9dac63dcb6f0eb0d51adb5f8e9396b9f272463
jane <jane@teamturing.com>
2023-02-20 13:44:49 +0900

    골드바흐

--- 골드바흐의추측_6588.py
+++ 골드바흐의추측_6588.py
@@ -1,3 +1,5 @@
+import sys
+
 n = 1000001
 Prime = [True] * n
 m = int(n ** 0.5)
@@ -8,15 +10,16 @@ for i in range(2, m + 1):
 
 
 while True:
-    n = int(input())
+    n = int(sys.stdin.readline().strip())
     Check = 0
     if n == 0:
         break
-    for i in range(3, 1000001):
-        if Prime[i] == True:
-            if Prime[n-i] == True:
-                print("%d = %d + %d"%(n, i, n-i))
-                Check = 1
-                break
+    for i in range(3, n // 2 + 1):
+        if Prime[i] == False:
+            continue
+        if Prime[n-i] == True:
+            print("%d = %d + %d"%(n, i, n-i))
+            Check = 1
+            break
     if Check == 0:
         print("Goldbach's conjecture is wrong.")
